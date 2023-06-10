5ee16ad895a552cc264f1d6bf36674f91ecd4aef
XimenaSanchezUwU <126804482+XimenaSanchezUwU@users.noreply.github.com>
2023-06-09 18:59:13 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let img;
+let myFont;
+function preload() {
+  img = loadImage('assets/A_tu_edad.png');
+  myFont = loadFont('assets/JMH Typewriter.otf');
+}
+
+function setup() {
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.style('display', 'block');
+  textFont(myFont);
+}
+
+function draw() {
+  background(0, 42, 54);
+  image(img, 0, 0, windowWidth, windowHeight);
+  textSize(65);
+  fill(21, 2, 0);
+  text('Yo ha tu edad ya', 120, 33);
+  fill(168, 13, 0);
+  textSize(45);
+  text('Tenía trabajo', random(32), 133);
+  textSize(40);
+  fill(255, 66, 66);
+  frameRate(2);
+  text('Tenía casa', random(532), 225);
+  fill(193, 242, 255);
+  frameRate(10);
+  fill(255, 15, 0);
+  text('Me había casado', random(132), 363);
+  frameRate(7);
+}
